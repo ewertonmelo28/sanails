8adb2c98fd703c8ccea95c0c479122ed424e7be1
ewertonmelo28 <2003861@aluno.univesp.br>
2021-10-14 01:52:26 -0300

    alteração do db

--- db/login.sql
+++ db/login.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `login` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `login`;
 -- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: login
@@ -49,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-10-13 22:19:06
+-- Dump completed on 2021-10-14  1:43:23

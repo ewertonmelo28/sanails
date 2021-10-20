d76baac73c86453645a637dbb93c8e2af24e383d
ewertonmelo28 <2003861@aluno.univesp.br>
2021-10-19 22:07:18 -0300

    Regra para remover data que já estão completas

--- view/salva_agendamento.php
+++ view/salva_agendamento.php
@@ -3,21 +3,36 @@ include("conexao.php");
 session_start();
 /* Inserção de dados na agenda  */
 
+
 if ($_POST['cliente_nome'] != null and $_POST['cliente_telefone'] != null) {
     $cliente_nome = $_POST['cliente_nome'];
     $cliente_telefone = $_POST['cliente_telefone'];
     $servico = $_POST['servico'];
     $data_agenda = $_POST['data_agenda'];
     $hora_agenda = $_POST['hora_agenda'];
+}else{
+    $_SESSION['erro'] = "<p>Informe Nome e Telefone !</p>";
+    header("Location: ../index.php?menuop=agendamento");
 }
 
 $alt_hora = "UPDATE agendamento.hora SET livre = 0 WHERE id_hora = $hora_agenda";
 $result_alt_hora = mysqli_query($conexao_agend, $alt_hora);
+
+$consult_hora = "SELECT SUM(livre) AS hora FROM hora WHERE fk_data = $data_agenda";
+$result_consult_hora = mysqli_query($conexao_agend, $consult_hora);
+$row_consult_hora = mysqli_fetch_array($result_consult_hora);
+
+switch ($row_consult_hora["hora"]) {
+    case '0':
+        $alt_data = "UPDATE agendamento.data SET livre = 0 WHERE id_data = $data_agenda";
+    $result_alt_hora = mysqli_query($conexao_agend, $alt_data);
+        break;
+}
+
+
 $input_agenda = "INSERT INTO agendamento.agenda(nome, telefone, servico, fk_data, fk_hora)
 VALUES ('$cliente_nome', '$cliente_telefone', '$servico', $data_agenda, $hora_agenda)";
 $result_input_agenda = mysqli_query($conexao_agend, $input_agenda);
-
-
 
 
 if (mysqli_insert_id($conexao_agend)) {
